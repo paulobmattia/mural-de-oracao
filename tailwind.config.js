f157352087b33ab35cad1b35460d1f082ebdcdf1
Paulo Mattia <seu-email-aqui@gmail.com>
2025-11-21 11:09:41 -0300

    Fix Dark Mode Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Essa linha obriga o site a obedecer o seu botão
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
